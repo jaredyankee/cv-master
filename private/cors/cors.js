6dd8c975661a82ff3d79c11aea4441eb6b9eb81f
Jared Yankee <jayankee401@gmail.com>
2026-09-08 07:38:17 -0400

    add referer to allowed origins
    prevents blocking of same-origin requests

--- private/cors/cors.js
+++ private/cors/cors.js
@@ -6,7 +6,8 @@ export const CORS = (event, page = null) => {
     const allowed = [
         'http://localhost:5173',
         'http://localhost:5174',
-        'https://cvmaster-jy.netlify.app'
+        'https://cvmaster-jy.netlify.app',
+        'https://cvmaster-jy.netlify.app/'
     ];
 
     console.log("Origin: ", event.headers.origin);
